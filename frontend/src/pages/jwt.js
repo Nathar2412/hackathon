ab14ffee2942eef8f9b15d22c9d56caf952de5e7
mdnathar-123 <mdnathar@digiclarity.in>
2025-03-08 12:56:08 +0530

    basic login page

---
+++ frontend/src/pages/jwt.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "../style.css";
+import CustomInput from "../components/Custominput";
+import Button from "../components/Button";
+// import CustomInput from "./components/CustomInput";
+// import Button from "./components/Button";
+
+export default class App extends Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+    handleChange = e => {
+
+        console.log(JSON.stringify(this.state))
+        this.setState({ [e.currentTarget.id]: e.currentTarget.value });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <form className="form">
+                    <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="text"
+                    />
+                    <CustomInput
+                        labelText="Password"
+                        id="password"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="password"
+                    />
+
+                    <Button type="button" color="primary" className="form__custom-button">
+                        Log in
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+}
